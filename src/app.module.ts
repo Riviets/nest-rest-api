fc5aa2afee8299d12877933bbf9cdc9d49dbe64e
Riviets <vladvladpobuta@gmail.com>
2025-08-17 07:32:50 +0300

    Implemented CORS and rate limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,36 @@ import { Module } from '@nestjs/common';
 import { ArticlesModule } from './articles/articles.module';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [ArticlesModule, UsersModule, DatabaseModule],
+  imports: [
+    ArticlesModule,
+    UsersModule,
+    DatabaseModule,
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 1000,
+        limit: 3,
+      },
+      {
+        name: 'long',
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
